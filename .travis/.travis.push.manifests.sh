4eb4e0823228c713607486079487e9a548e4fe71
Travis CI <travis@travis-ci.org>
2018-06-01 13:03:54 +0200

    Issue #11: copy generated files to tmp and then back

--- .travis/.travis.push.manifests.sh
+++ .travis/.travis.push.manifests.sh
@@ -6,8 +6,8 @@ BRANCH="dist"
 REPO="Jiri-Kremser/spark-operator"
 
 generate() {
-  cat manifest/openshift/{rbac-openshift,operator}.yaml > openshift-spark-operator.yaml
-  cat manifest/kubernetes/{rbac-kubernetes,operator}.yaml > k8s-spark-operator.yaml
+  cat manifest/openshift/{rbac-openshift,operator}.yaml > /tmp/openshift-spark-operator.yaml
+  cat manifest/kubernetes/{rbac-kubernetes,operator}.yaml > /tmp/k8s-spark-operator.yaml
 }
 
 setup-git() {
@@ -18,8 +18,19 @@ setup-git() {
   set -x
 }
 
+switch-branch() {
+  git fetch origin +refs/heads/$BRANCH:refs/remotes/origin/$BRANCH
+  git remote set-branches --add origin $BRANCH
+  git checkout --track -b $BRANCH origin/$BRANCH
+  git pull
+}
+
+copy() {
+  cp /tmp/openshift-spark-operator.yaml .
+  cp /tmp/k8s-spark-operator.yaml .
+}
+
 commit() {
-  git checkout -b $BRANCH
   git add -A
   git commit -m "Travis build: $TRAVIS_BUILD_NUMBER"
 }
@@ -33,6 +44,8 @@ push() {
 main() {
   setup-git
   generate
+  switch-branch
+  copy
   commit
   push
 }
