2f1a90caab5a8f6de2c7da2050a3c49f0c8f546c
Jirka Kremser <jkremser@redhat.com>
2018-06-01 12:22:50 +0200

    Issue #11: automatic git push back to the dist branch

--- .travis/.travis.push.manifests.sh
+++ .travis/.travis.push.manifests.sh
@@ -3,6 +3,38 @@
 set -xe
 
 BRANCH="dist"
+REPO="Jiri-Kremser/spark-operator"
 
-cat manifest/openshift/{rbac-openshift,operator}.yaml > openshift-spark-operator.yaml
-cat manifest/kubernetes/{rbac-kubernetes,operator}.yaml > k8s-spark-operator.yaml
+generate() {
+  cat manifest/openshift/{rbac-openshift,operator}.yaml > openshift-spark-operator.yaml
+  cat manifest/kubernetes/{rbac-kubernetes,operator}.yaml > k8s-spark-operator.yaml
+}
+
+setup-git() {
+  git config --global user.email "travis@travis-ci.org"
+  git config --global user.name "Travis CI"
+  set +x
+  git remote add upstream https://${GH_TOKEN}@github.com/$REPO.git > /dev/null 2>&1
+  set -x
+}
+
+commit() {
+  git checkout $BRANCH
+  git add -A
+  git commit -m "Travis build: $TRAVIS_BUILD_NUMBER"
+}
+
+push() {
+  set +x
+  git push --quiet --set-upstream upstream $BRANCH
+  set -x
+}
+
+main() {
+  setup-git
+  generate
+  commit
+  push
+}
+
+main
