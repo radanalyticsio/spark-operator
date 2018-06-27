8e6d52b097c85adf43f5eb7d5b93f6e46bc1cf32
Jirka Kremser <jkremser@redhat.com>
2018-06-27 14:49:32 +0200

    Comments & making slim the default

--- .travis/.travis.release.images.sh
+++ .travis/.travis.release.images.sh
@@ -9,9 +9,9 @@ IMAGE="${IMAGE:-spark-operator}"
 if [[ "${TRAVIS_TAG}" =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
 
   # push the latest and ${TRAVIS_TAG} images
-  docker tag $OWNER/$IMAGE:slim $OWNER/$IMAGE:${TRAVIS_TAG}-slim
+  docker tag $OWNER/$IMAGE:slim $OWNER/$IMAGE:${TRAVIS_TAG}
   docker tag $OWNER/$IMAGE:centos $OWNER/$IMAGE:${TRAVIS_TAG}-centos
-  docker push $OWNER/$IMAGE:${TRAVIS_TAG}-slim
+  docker push $OWNER/$IMAGE:${TRAVIS_TAG}
   docker push $OWNER/$IMAGE:${TRAVIS_TAG}-centos
 else
   echo "Not doing the docker push, because the tag '${TRAVIS_TAG}' is not of form x.y.z.Final"
