001fbd3ae91047dd6ba2a843a6aac21fc858b6f8
Jirka Kremser <jkremser@redhat.com>
2018-06-29 18:01:24 +0200

    Colors 4 travis

--- test/lib/util/text.sh
+++ test/lib/util/text.sh
@@ -83,7 +83,7 @@ function os::text::clear_string() {
 
 # os::text::internal::is_tty determines if we are outputting to a TTY
 function os::text::internal::is_tty() {
-	[[ -t 1 && -n "${TERM:-}" ]]
+	[[ "${TRAVIS:-}" = "true" ]] ||  [[ -t 1 && -n "${TERM:-}" ]]
 }
 readonly -f os::text::internal::is_tty
 
