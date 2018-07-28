361ee9f6e7edc472774fab9d6af0e7271227a90b
Jirka Kremser <jkremser@redhat.com>
2018-07-28 16:28:13 +0200

    auto-pushing of images also to quay.io

--- .travis/.travis.release.images.sh
+++ .travis/.travis.release.images.sh
@@ -4,22 +4,62 @@ set -xe
 
 OWNER="${OWNER:-jkremser}"
 IMAGE="${IMAGE:-spark-operator}"
+[ "$TRAVIS_BRANCH" = "master" -a "$TRAVIS_PULL_REQUEST" = "false" ] && LATEST=1
 
-# if building the release tag
-if [[ "${TRAVIS_TAG}" =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
+main() {
+  if [[ "$LATEST" = "1" ]]; then
+    echo "Pushing the :latest and :latest-centos images to docker.io and quay.io"
+    loginDockerIo
+    pushLatestImages
+    loginQuayIo
+    pushLatestImages
+  else if [[ "${TRAVIS_TAG}" =~ ^[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
+    echo "Pushing the '${TRAVIS_TAG}' and :latest-released images to docker.io and quay.io"
+    buildReleaseImages
+    loginDockerIo
+    pushReleaseImages
+    loginQuayIo
+    pushReleaseImages
+  else
+    echo "Not doing the docker push, because the tag '${TRAVIS_TAG}' is not of form x.y.z.Final"
+    echo "and also it's not a build of the master branch"
+  fi
+}
 
+loginDockerIo() {
+  set +x
+  docker login -u "$DOCKER_USERNAME" -p "$DOCKER_PASSWORD"
+  set -x
+}
+
+loginQuayIo() {
+  set +x
+  docker login -u "$DOCKER_USERNAME" -p "$QUAY_PASSWORD" quay.io
+  set -x
+}
+
+pushLatestImages() {
+  make image-publish-all
+  docker logout
+}
+
+buildReleaseImages() {
   # build centos image
   make package image-build
 
-  # tag and push the latest-released and ${TRAVIS_TAG} images (and also -centos images)
   docker tag $OWNER/$IMAGE:slim $OWNER/$IMAGE:${TRAVIS_TAG}
   docker tag $OWNER/$IMAGE:slim $OWNER/$IMAGE:latest-released
   docker tag $OWNER/$IMAGE:centos $OWNER/$IMAGE:${TRAVIS_TAG}-centos
   docker tag $OWNER/$IMAGE:centos $OWNER/$IMAGE:latest-released-centos
+}
+
+pushReleaseImages() {
+  # push the latest-released and ${TRAVIS_TAG} images (and also -centos images)
   docker push $OWNER/$IMAGE:${TRAVIS_TAG}
   docker push $OWNER/$IMAGE:latest-released
   docker push $OWNER/$IMAGE:${TRAVIS_TAG}-centos
   docker push $OWNER/$IMAGE:latest-released-centos
-else
-  echo "Not doing the docker push, because the tag '${TRAVIS_TAG}' is not of form x.y.z.Final"
-fi
+  docker logout
+}
+
+main
