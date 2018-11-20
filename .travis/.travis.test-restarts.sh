4b4106d1b6147b900c3545de2c690e5fe669c994
Jirka Kremser <jkremser@redhat.com>
2018-11-20 15:57:35 +0100

    tests: redefine operator_pod for logs

--- .travis/.travis.test-restarts.sh
+++ .travis/.travis.test-restarts.sh
@@ -78,6 +78,7 @@ testCreateOperator() {
     os::cmd::try_until_text "${BIN} get crd" 'sparkclusters.radanalytics.io'
   fi
   sleep 10
+  export operator_pod=`${BIN} get pod -l app.kubernetes.io/name=spark-operator -o='jsonpath="{.items[0].metadata.name}"' | sed 's/"//g'`
 }
 
 testCreateCluster() {
@@ -90,9 +91,9 @@ testCreateCluster() {
 
 testKillOperator() {
   info
-  local pod=`${BIN} get pod -l app.kubernetes.io/name=spark-operator -o='jsonpath="{.items[0].metadata.name}"' | sed 's/"//g'`
-  os::cmd::expect_success_and_text "${BIN} delete pod $pod" 'pod "?'$pod'"? deleted' && \
-  sleep 7
+  os::cmd::expect_success_and_text "${BIN} delete pod $operator_pod" 'pod "?'$operator_pod'"? deleted' && \
+  sleep 10
+  export operator_pod=`${BIN} get pod -l app.kubernetes.io/name=spark-operator -o='jsonpath="{.items[0].metadata.name}"' | sed 's/"//g'`
 }
 
 testScaleCluster() {
