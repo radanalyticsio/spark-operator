b95962310a48517c9728d9e5503626f46a04a40b
Jirka Kremser <jkremser@redhat.com>
2019-01-15 14:54:49 +0100

    New metric for # of full reconciliations + renaming some existing metrics + refactoring

--- src/main/java/io/radanalytics/operator/cluster/RunningClusters.java
+++ src/main/java/io/radanalytics/operator/cluster/RunningClusters.java
@@ -1,51 +1,42 @@
 package io.radanalytics.operator.cluster;
 
-import io.prometheus.client.Counter;
-import io.prometheus.client.Gauge;
+import io.prometheus.client.log4j.InstrumentedAppender;
 import io.radanalytics.types.RCSpec;
 import io.radanalytics.types.SparkCluster;
+import org.apache.log4j.Logger;
 
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
 
+import static io.radanalytics.operator.cluster.MetricsHelper.runningClusters;
+import static io.radanalytics.operator.cluster.MetricsHelper.startedTotal;
+import static io.radanalytics.operator.cluster.MetricsHelper.workers;
+
+
 
 public class RunningClusters {
 
-    public static final Gauge runningClusters = Gauge.build()
-            .name("operator_running_clusters")
-            .help("Spark clusters that are currently running.")
-            .register();
-
-    public static final Gauge workers = Gauge.build()
-            .name("operator_running_workers")
-            .help("Number of workers per cluster name.")
-            .labelNames("cluster")
-            .register();
-
-    public static final Gauge startedTotal = Gauge.build()
-            .name("operator_started_total")
-            .help("Spark clusters has been started by operator.")
-            .register();
-
     private final Map<String, SparkCluster> clusters;
+    private final String namespace;
 
-    public RunningClusters() {
+    public RunningClusters(String namespace) {
         clusters = new HashMap<>();
-        runningClusters.set(0);
+        this.namespace = namespace;
+        runningClusters.labels(namespace).set(0);
     }
 
     public void put(SparkCluster ci) {
-        runningClusters.inc();
-        startedTotal.inc();
-        workers.labels(ci.getName()).set(Optional.ofNullable(ci.getWorker()).orElse(new RCSpec()).getInstances());
+        runningClusters.labels(namespace).inc();
+        startedTotal.labels(namespace).inc();
+        workers.labels(ci.getName(), namespace).set(Optional.ofNullable(ci.getWorker()).orElse(new RCSpec()).getInstances());
         clusters.put(ci.getName(), ci);
     }
 
     public void delete(String name) {
         if (clusters.containsKey(name)) {
-            runningClusters.dec();
-            workers.labels(name).set(0);
+            runningClusters.labels(namespace).dec();
+            workers.labels(name, namespace).set(0);
             clusters.remove(name);
         }
     }
@@ -55,9 +46,9 @@ public class RunningClusters {
     }
 
     public void resetMetrics() {
-        startedTotal.set(0);
-        workers.clear();
-        startedTotal.set(0);
+        startedTotal.labels(namespace).set(0);
+        clusters.forEach((c, foo) -> workers.labels(c, namespace).set(0));
+        startedTotal.labels(namespace).set(0);
     }
 
 }
