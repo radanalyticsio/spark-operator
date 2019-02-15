373df9e45f124c6103da8e3ecde703fbf278f142
Michael McCune <msm@redhat.com>
2019-02-15 15:13:34 -0500

    change default spark image mechanics
    This change makes it possible to set the default spark image through an
    environment variable in the operator container. It also changes the
    default image specified inside the code to
    `quay.io/radanalyticsio/openshift-spark:latest` and removes the default
    image ref from the schema spec.

--- src/main/java/io/radanalytics/operator/Constants.java
+++ src/main/java/io/radanalytics/operator/Constants.java
@@ -2,8 +2,16 @@ package io.radanalytics.operator;
 
 public class Constants {
 
-    public static String DEFAULT_SPARK_IMAGE = "quay.io/jkremser/openshift-spark:2.4.0";
+    public static String DEFAULT_SPARK_IMAGE = "quay.io/radanalyticsio/openshift-spark:latest";
     public static final String OPERATOR_TYPE_UI_LABEL = "ui";
     public static final String OPERATOR_TYPE_MASTER_LABEL = "master";
     public static final String OPERATOR_TYPE_WORKER_LABEL = "worker";
+
+    public static String getDefaultSparkImage() { 
+        String ret = DEFAULT_SPARK_IMAGE;
+        if (System.getenv("DEFAULT_SPARK_CLUSTER_IMAGE") != null) {
+            ret = System.getenv("DEFAULT_SPARK_CLUSTER_IMAGE");
+        }
+        return ret;
+    }
 }
