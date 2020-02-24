47151777104f96fe99e79905c1e5b5e718d96fcf
Trevor McKay <tmckay@redhat.com>
2020-02-24 10:31:27 -0500

    Use radanalyticsio/openshift-spark:2.4-latest or 2.4.5-2 for apps (#285)
    some of the app examples are using older versions of spark 2.3.
    on current versions of openshift, spark-on-k8s does not work
    for spark versions before 2.4.5

--- src/main/java/io/radanalytics/operator/Constants.java
+++ src/main/java/io/radanalytics/operator/Constants.java
@@ -3,7 +3,7 @@ package io.radanalytics.operator;
 public class Constants {
 
     public static String DEFAULT_SPARK_IMAGE = "quay.io/radanalyticsio/openshift-spark:2.4-latest";
-    public static String DEFAULT_SPARK_APP_IMAGE = "quay.io/jkremser/openshift-spark:2.3-latest";
+    public static String DEFAULT_SPARK_APP_IMAGE = "quay.io/radanalyticsio/openshift-spark:2.4-latest";
     public static final String OPERATOR_TYPE_UI_LABEL = "ui";
     public static final String OPERATOR_TYPE_MASTER_LABEL = "master";
     public static final String OPERATOR_TYPE_WORKER_LABEL = "worker";
